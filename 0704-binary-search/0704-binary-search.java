8012de6050925328f11affe155f2ca769210def9
Gaurav Kumar <gaurav.kumaar62@gmail.com>
2026-08-14 15:26:44 +0530

    Time: 0 ms (100%), Space: 47.7 MB (98.72%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        return needed(nums,target,0,n-1);
+    }
+    public int needed(int[] nums, int target, int lo , int hi){
+        if (lo>hi) return -1;
+        int mid= lo + (hi-lo)/2;
+        if(nums[mid]== target) return mid;
+        else if(nums[mid]>target)  return needed(nums, target, lo,mid-1);
+        else return needed(nums, target,mid+1 ,hi);
+    }
+}
